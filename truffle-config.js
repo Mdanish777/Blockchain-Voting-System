a63858ffbe0b0da7504613d91a86e325b0cef7fd
Mdanish777 <danishshamim777@gmail.com>
2022-12-13 19:02:24 +0530

    2nd commit

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
-  networks: {
+  networks: { // just for test
     development: {
       host: "127.0.0.1",
       port: 7545,
